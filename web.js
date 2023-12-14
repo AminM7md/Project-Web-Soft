cf85aa93234f5e6cdb3d56afada88cd6d112cde7
bodisalman <114391217+bodisalman@users.noreply.github.com>
2023-12-14 09:35:51 +0200

    Add files via upload

---
+++ web.js
@@ -0,0 +1,5 @@
+function test() {
+    document.getElementById('test').innerHTML = 'BMW i8'
+    document.getElementById('testt').innerHTML =
+        'Legacy BMW i8 Coupe and BMW i8 RoadsterThe BMW i8 Coupe and BMW i8 Roadster are high-performance plugi n hybrid sportscars. Unlike all-electric vehicles the i8 features both a synchronous electric motor and a 1.5 liter TwinPower Turbo engine giving drivers the flexibility to select between gasoline only electric only  or an efficient blend of both'
+}
